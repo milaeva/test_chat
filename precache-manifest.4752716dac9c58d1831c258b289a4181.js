f4a6c3760aaa8082879290a04982c2b52f7bbaf9
Vita Mileva <milayevavita27@gmail.com>
2019-09-02 09:19:16 +0300

    Updates

---
+++ precache-manifest.4752716dac9c58d1831c258b289a4181.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca4333d6f2b845301d130bfaa4345580",
+    "url": "/test_chat/index.html"
+  },
+  {
+    "revision": "8374e40e5a8f4fd8d744",
+    "url": "/test_chat/static/css/main.3052c805.chunk.css"
+  },
+  {
+    "revision": "d7cf488f7adb03f3c446",
+    "url": "/test_chat/static/js/2.4495dcc6.chunk.js"
+  },
+  {
+    "revision": "8374e40e5a8f4fd8d744",
+    "url": "/test_chat/static/js/main.dbbf4daa.chunk.js"
+  },
+  {
+    "revision": "cf7f252d72f69e48ace1",
+    "url": "/test_chat/static/js/runtime~main.d5e7fa9d.js"
+  }
+]);
